b0aa52a7252466117469b64a5b009b3e8982f4fa
“Arka <arkabhattacharyya.brc@gmail.com>
2023-07-03 12:16:16 +0530

    create sass file and make post schema for create posts

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const port = 6500;
 //require express ejs layout
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
+const sassMiddleware = require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src : './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'expanded',
+    prefix: '/css'
+}))
 
 app.use(express.urlencoded());
  app.use(cookieParser());
@@ -17,14 +30,29 @@ app.use(expressLayouts);
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
 
-
-
-//user express router
-app.use('/',require('./routes'));
-
 //set up the ejs view engine
 app.set('view engine','ejs');
 app.set('views', './views');
+// mongo store is used to store the session cookie in the db
+app.use(session({
+        name: 'codeial',
+        secret: 'somethings',
+        saveUninitialized: false,
+        resave: false,
+        cookie: {
+            maxAge: (1000 * 40 * 100)
+        },
+        store: MongoStore.create({ mongoUrl: 'mongodb://localhost:27017/codeial_development' })
+       
+        
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+
+//user express router
+app.use('/',require('./routes'));
 
 app.listen(port, function(err){
   
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const routes = express.Router();
+const passport = require('passport');
 const userController = require('../controllers/users_controller');
 
 
-routes.get('/profile', userController.profile);
+routes.get('/profile', passport.checkAuthentication, userController.profile);
 
 routes.get('/sign-up', userController.signUp);
 routes.get('/sign-in', userController.signIn);
 
 routes.post('/create', userController.create);
+
+routes.post('/create-session', passport.authenticate(
+    'local',
+    {failureRedirect: '/users/sign-in'}, 
+    ) , userController.createSession);
+
+routes.get('/sign-out', userController.destroySession);
 
 
 module.exports = routes;
